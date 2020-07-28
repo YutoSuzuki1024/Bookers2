c0179c83e5ab6ae481d379683a7d6a150e9f4222
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-07-28 19:04:44 +0900

    Fourth Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,13 @@
 class BooksController < ApplicationController
 	before_action :authenticate_user!
-	def top
-	end
-
 	def new
+		@book = Book.new
 	end
 
 	def create
-		
+		book = Book.new(book_params)
+		book.save
+		redirect_to book_path(book.id)
 	end
 
 	def index
@@ -15,7 +15,8 @@ class BooksController < ApplicationController
 	end
 
 	def show
-		
+		@book = Book.find(params[:id])
+		@user = User.find(params[:id])
 	end
 
 	def edit
@@ -28,5 +29,10 @@ class BooksController < ApplicationController
 
 	def destroy
 		
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  # get 'users/index'
   # get 'users/edit'
   devise_for :users
-
+  resources :books
+  resources :users
   # constraints -> request { request.session[:id].present? } do
   # 	root 'users#show'
   # end
-
-  root 'books#top'
-  # root 'users#show'
-  
-  resources :books
-  resources :users, only: [:show, :index]
+  root 'users#top'
   get 'home/about' => 'home#about'
 end
