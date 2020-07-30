d0e4feeb4a8add3cdfc11021772ca45a073a5926
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-07-30 20:46:35 +0900

    6th Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,17 +7,25 @@ class BooksController < ApplicationController
 	def create
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
-		binding.pry
-		@book.save
-		redirect_to book_path(@book.id)
+		if @book.save
+			redirect_to book_path(@book.id)
+		else
+			render 'books/index'
+		end
 	end
 
 	def index
+		@user = User.find(current_user.id)
+		@books = Book.all
+		@book = Book.new
 	end
 
 	def show
 		@book = Book.find(params[:id])
-		@user = User.find(params[:id])
+		# @user = @book.user
+		@books = Book.all
+		@user = User.find(current_user.id)
+		#http://localhost:3000/books/3以降で反応する（1,2はuser_idがnil)
 	end
 
 	def edit
